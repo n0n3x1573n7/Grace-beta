4ee317d3db1e850ddcddf3b8a6eaf928dd5a8983
n0n3x1573n7 <n0n3x1573n7@users.noreply.github.com>
2019-05-20 19:50:34 +0900

    changed mistake to inequality... again!

--- Grace.py
+++ Grace.py
@@ -225,7 +225,7 @@ async def 신청(message):
     player=author(message)
 
     if current_game.additional_opened==False or\
-       (current_game.time-current_time()>timedelta(minutes=10) and current_time()-current_game.time>timedelta(hours=1)):
+       (timedelta(minutes=-10)<current_time()-current_game.time<timedelta(hours=1)):
         await message.channel.send("신청이 마감되었습니다. 추가신청을 기다려주세요.")
         return
 
