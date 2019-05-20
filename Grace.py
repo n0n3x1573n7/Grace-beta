3bfb82defb7e2ac12a3035b49abe5eacfa633abd
n0n3x1573n7 <n0n3x1573n7@users.noreply.github.com>
2019-05-20 19:22:06 +0900

    fixed conditions on registration

--- Grace.py
+++ Grace.py
@@ -224,8 +224,8 @@ async def 신청(message):
 
     player=author(message)
 
-    if (current_game.time-current_time()<timedelta(minutes=10) and not current_game.additional_opened)\
-       or (current_time()-current_game.time>=timedelta(hours=1)):
+    if current_game.additional_opened==False or\
+       (current_game.time-current_time()>timedelta(minutes=10) and current_time()-current_game.time<timedelta(hours=1)):
         await message.channel.send("신청이 마감되었습니다. 추가신청을 기다려주세요.")
         return
 
