ccdfd406716b14b2ab0f7a1be1a31479a9481d8c
n0n3x1573n7 <n0n3x1573n7@users.noreply.github.com>
2019-05-24 00:38:18 +0900

    Update Grace.py

--- Grace.py
+++ Grace.py
@@ -345,7 +345,7 @@ async def 신청반려(message):
 #도움말
 @client.command()
 async def 도움말(ctx):
-    if TESTING and message.channel.id!=channels['내전신청']: return
+    if TESTING and ctx.channel.id!=channels['내전신청']: return
     embed = discord.Embed(title="Grace bot", description="그레이스 클랜 봇입니다.", color=0xeee657)
     embed.add_field(name="\u200B",value="\u200B",inline=False)
     embed.add_field(name="전체 서버",value="\u200B",inline=False)
