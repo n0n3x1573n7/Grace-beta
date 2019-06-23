ab31296272d32f99ff824c122ea66252818ea2bf
n0n3x1573n7 <n0n3x1573n7@users.noreply.github.com>
2019-06-23 21:49:43 +0900

    Update Grace_game.py

--- Grace_game.py
+++ Grace_game.py
@@ -367,7 +367,10 @@ async def 임의신청(message):
     players=message.message.content.split()[1:]
 
     for plr in players:
-        player=client.get_user(int(plr[2:-1]))
+        try:
+            player=client.get_user(int(plr[2:-1]))
+        except:
+            pass
         if current_game.add_player(player):
             await message.channel.send("{}님의 임의신청이 완료되었습니다.".format(player.mention))
         else:
