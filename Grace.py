b663e35050d7669b865b82269caceaadc98dc819
Whitepp <dana_bak@naver.com>
2019-07-17 20:28:24 +0900

    Update Grace.py

--- Grace.py
+++ Grace.py
@@ -117,10 +117,10 @@ async def on_message(message):
         embed.set_thumbnail(url=thumbnaillink)
         embed.set_author(name=battletag)
         embed.add_field(name="직책", value=roleimage + role, inline=True)
-        print(arena)
         if arena is not "X":
             embed.add_field(name="Grace Arena", value=":trophy: 제 " + arena + "회 우승", inline=True)
-        embed.add_field(name="Grace League", value=":first_place: 제 " + league_first + "회 우승, :second_place:제 " +
+        if league_first is not "X" | league_second is not "X":
+            embed.add_field(name="Grace League", value=":first_place: 제 " + league_first + "회 우승, :second_place:제 " +
                                                    league_second + "회 준우승", inline=True)
 
         await channel.send(embed=embed)
